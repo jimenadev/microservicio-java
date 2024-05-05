b1d94e5f6a803d0e714964054bfd72b0a3bc3669
jimena <dev.jimena.silva@gmail.com>
2024-05-05 12:52:43 -0400

    se agrega sonarcloud con quiebre de pipeline por quality gate

--- src/main/java/com/example/testingweb/HomeController.java
+++ src/main/java/com/example/testingweb/HomeController.java
@@ -15,6 +15,30 @@ public class HomeController {
 		String pass4 ="1234";
 		String pass5 ="1234";
 		String pass6 ="1234";
+		String pass7 ="1234";
+		String pass8 ="1234";
+		String pass9 ="1234";
+		String pass10 ="1234";
+		String pass11 ="1234";
+		String pass12 ="1234";
+		String pass13 ="1234";
+		String pass14 ="1234";
+		String pass15 ="1234";
+		String pass16 ="1234";
+		String pass17 ="1234";
+		String pass18 ="1234";
+		String pass19 ="1234";
+		String pass20 ="1234";
+		String pass21 ="1234";
+		String pass22 ="1234";
+		String pass23 ="1234";
+		String pass24 ="1234";
+		String pass25 ="1234";
+		String pass26 ="1234";
+		String pass27 ="1234";
+		String pass28 ="1234";
+
+
 		return "Hello, World";
 	}
 
