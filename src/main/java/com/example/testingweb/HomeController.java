c8895c0760289dbf476444dc165cfce82ed1eb2c
jimena <dev.jimena.silva@gmail.com>
2024-05-07 21:32:25 -0400

    se agrega services en deploy.yml

--- src/main/java/com/example/testingweb/HomeController.java
+++ src/main/java/com/example/testingweb/HomeController.java
@@ -9,24 +9,6 @@ public class HomeController {
 
 	@RequestMapping("/")
 	public @ResponseBody String greeting() {
-		String pass ="1234";
-		String pass2 ="1234";
-		String pass3 ="1234";
-		String pass4 ="1234";
-		String pass5 ="1234";
-		String pass6 ="1234";
-		String pass7 ="1234";
-		String pass8 ="1234";
-		String pass9 ="1234";
-		String pass10 ="1234";
-		String pass11 ="1234";
-		String pass12 ="1234";
-		String pass13 ="1234";
-		String pass14 ="1234";
-		String pass15 ="1234";
-		String pass16 ="1234";
-
-
 		return "Hello, World";
 	}
 
