c9920dce96ad3c24bdead9434826124233f4a9fa
jimena <dev.jimena.silva@gmail.com>
2024-05-10 20:59:45 -0400

    ejecutar test rendimeitno

--- e2e/testk6-rendimiento.js
+++ e2e/testk6-rendimiento.js
@@ -1,17 +1,25 @@
 import http from 'k6/http';
 import { check } from 'k6';
 
+const headers = {
+  'Authorization': `Bearer dbe68ca2c444344af8603e06fe3f7288082a3d2d30c3e1dc36255fb290044a07`,
+  'Project-ID': 3696227,
+};
+
 export const options = {
-  stages:[
-    {
-        duration: '10s',
-        target: 30,
-    }
-  ]
-}
+  vus: 30,
+  duration: '10s',
+  ext:{
+      loadimpact:{
+          name: "Test de rendimiento de microservicio-java",
+          projectID: 3696227,
+          staticIP: true,
+      }
+  }
+};
 
 export default function() {
-  const response = http.get("http://localhost:8085/greeting");
+  const response = http.get("http://localhost:8085/greeting", headers);
 
   // Aserción para verificar el código de estado
   check(response, {
